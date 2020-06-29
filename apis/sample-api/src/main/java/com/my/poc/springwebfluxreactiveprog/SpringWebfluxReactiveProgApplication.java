02908cc36bee4040cb641dff818932c11be1978a
Poornima Rajendhran <poornimarajendhran@Poornimas-Air.home>
2020-06-29 06:52:58 -0400

    Spring boot domain driven design (DDD) skeleton setup

---
+++ apis/sample-api/src/main/java/com/my/poc/springwebfluxreactiveprog/SpringWebfluxReactiveProgApplication.java
@@ -0,0 +1,13 @@
+package com.my.poc.springwebfluxreactiveprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxReactiveProgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxReactiveProgApplication.class, args);
+	}
+
+}
